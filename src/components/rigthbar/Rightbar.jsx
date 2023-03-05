9136d5074ac2272f15e96b6ceb82e12d8b705f07
Cispas_M <crispasmakanani@gmail.com>
2023-03-05 10:47:58 +0200

    update

--- src/components/rigthbar/Rightbar.jsx
+++ src/components/rigthbar/Rightbar.jsx
@@ -1,6 +1,9 @@
-import { Avatar, Box, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material'
+import { Avatar, Box, Card, Divider, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material'
 import React from 'react'
 import { Typography } from '@mui/material';
+import firstPic from '../../static/firstPic.jpg'
+import secondPic from '../../static/secondPic.jpg'
+import thirdPic from '../../static/thirdPic.jpg'
 
 const Rightbar = () => {
   return (
@@ -11,26 +14,38 @@ const Rightbar = () => {
      <List sx={{ height:'100%',width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       <ListItem alignItems="flex-end">
         <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          <Avatar sx={{height: 80, width: 80}} variant='squaire' alt='Remy sharp' src={firstPic}/>
         </ListItemAvatar>
         <ListItemText
-          primary="Brunch this weekend?"
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: 'inline' }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </React.Fragment>
-          }
+          secondary= {" — I'll be in your neighborhood doing errands this…"}
         />
       </ListItem>
+
+      <ListItem alignItems="flex-end">
+        <ListItemAvatar>
+          <Avatar sx={{height: 80, width: 80}} variant='squaire' alt='Remy sharp' src={secondPic}/>
+        </ListItemAvatar>
+        <ListItemText
+          secondary= {" — I'll be in your neighborhood doing errands this…"}
+        />
+      </ListItem>
+
+      <ListItem alignItems="flex-end">
+        <ListItemAvatar>
+          <Avatar sx={{height: 80, width: 80}} variant='squaire' alt='Remy sharp' src={thirdPic}/>
+        </ListItemAvatar>
+        <ListItemText
+          secondary= {" — I'll be in your neighborhood doing errands this…"}
+        />
+      </ListItem>
+
+
+      <Divider variant='insert' component={'li'}></Divider>
      </List>
+     <Typography align='center' bgcolor={'black'} color="white">
+        About Us
+    </Typography>
+    <Card cardImage={secondPic}/>
     </Box>
   )
 }
